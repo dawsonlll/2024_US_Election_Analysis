6c7c187b081b3d5c183ac7c58147bc352a105aef
Dawson Li <dawsonli@mail.utoronto.ca>
2024-11-02 03:12:56 -0400

    Update the analysis data tests
    added tests for Harris

--- scripts/03-test_analysis_data.R
+++ scripts/03-test_analysis_data.R
@@ -17,45 +17,95 @@ setwd("/Users/dingshuo/Desktop/paper2 draft/2024_US_Election_Analysis/")
 library(tidyverse)
 library(testthat)
 
-data <- read_csv("data/02-analysis_data/analysis_data.csv")
+trump_data <- read_csv("data/02-analysis_data/analysis_trump_data.csv")
 
 
-#### Test data ####
-# Test that the dataset has 151 rows - there are 151 divisions in Australia
+#### Test trump_data ####
+# Test that the dataset has 1663 rows",
 test_that("dataset has expected rows", {
-  expect_equal(nrow(data), 393)  
+  expect_equal(nrow(trump_data), 1662)  
 })
 
 
 test_that("dataset has expected column", {
-  expect_equal(ncol(data), 52)  
+  expect_equal(ncol(trump_data), 53)  
 })
 
 
 test_that("'pollster' is character", {
-  expect_type(data$pollster, "character")
+  expect_type(trump_data$pollster, "character")
 })
 
 
 # Test that the 'state' column is character type
 test_that("'state' is character", {
-  expect_type(data$state, "character")
+  expect_type(trump_data$state, "character")
 })
 
 
 # Test that 'candidate_name' contains only "Donald Trump"
 test_that("'candidate_name' contains only 'Donald Trump'", {
-  expect_true(all(data$candidate_name == "Donald Trump"))
+  expect_true(all(trump_data$candidate_name == "Donald Trump"))
 })
 
 # Test that 'numeric_grade' is numeric and all values are 3 or higher
 test_that("'numeric_grade' is numeric and >= 2.7", {
-  expect_type(data$numeric_grade, "double")
-  expect_true(all(data$numeric_grade >= 2.7))
+  expect_type(trump_data$numeric_grade, "double")
+  expect_true(all(trump_data$numeric_grade >= 2.7))
 })
 
 # Test that 'end_date' is properly formatted as a Date object
 test_that("'end_date' is Date type", {
-  expect_s3_class(data$end_date, "Date")
+  expect_s3_class(trump_data$end_date, "Date")
 })
+
+
+
+
+harris_data <- read_csv("data/02-analysis_data/analysis_harris_data.csv")
+
+
+#### Test harris_data ####
+# Test that the dataset has 733 rows",
+test_that("dataset has expected rows", {
+  expect_equal(nrow(harris_data), 733)  
+})
+
+
+test_that("dataset has expected column", {
+  expect_equal(ncol(harris_data), 53)  
+})
+
+
+test_that("'pollster' is character", {
+  expect_type(harris_data$pollster, "character")
+})
+
+
+# Test that the 'state' column is character type
+test_that("'state' is character", {
+  expect_type(harris_data$state, "character")
+})
+
+
+# Test that 'candidate_name' contains only "Kamala Harris"
+test_that("'candidate_name' contains only 'Kamala Harris", {
+  expect_true(all(harris_data$candidate_name == "Kamala Harris"))
+})
+
+# Test that 'numeric_grade' is numeric and all values are 3 or higher
+test_that("'numeric_grade' is numeric and >= 2.7", {
+  expect_type(harris_data$numeric_grade, "double")
+  expect_true(all(harris_data$numeric_grade >= 2.7))
+})
+
+# Test that 'end_date' is properly formatted as a Date object
+test_that("'end_date' is Date type", {
+  expect_s3_class(harris_data$end_date, "Date")
+})
+
+
+
+
+
 
